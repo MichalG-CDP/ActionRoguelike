174eba636044104c3a9609b264b114cb4aac79ae
MichalG-CDP <michal.grzeszczak@cdprojektred.com>
2023-12-22 08:40:13 +0100

    Explosive Barrel implemented

--- Source/ActionRoguelike/Public/SExplosiveBarrel.h
+++ Source/ActionRoguelike/Public/SExplosiveBarrel.h
@@ -6,21 +6,32 @@
 #include "GameFramework/Actor.h"
 #include "SExplosiveBarrel.generated.h"
 
+
+class UStaticMeshComponent;
+class URadialForceComponent;
+
+
 UCLASS()
 class ACTIONROGUELIKE_API ASExplosiveBarrel : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	ASExplosiveBarrel();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(VisibleAnywhere)
+		TObjectPtr<UStaticMeshComponent> MeshComp;
+
+	UPROPERTY(VisibleAnywhere)
+		TObjectPtr<URadialForceComponent> ForceComp;
+
+	virtual void PostInitializeComponents() override;
+
+	// Must be marked with ufunction in order to 'bind' the event
+	UFUNCTION()
+	void OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 };
